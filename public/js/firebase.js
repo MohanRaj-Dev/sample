14ff8d03db8e0f1cda56241174165c0dd7bf386c
Mohan <85388246+MohanRaj-Dev@users.noreply.github.com>
2023-09-23 22:32:40 +0530

    Add files via upload

---
+++ public/js/firebase.js
@@ -0,0 +1,19 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyDl1OYKXlZHCCCNc4ySGHUivvuU-epcPwI",
+
+    authDomain: "scriptlook-1e871.firebaseapp.com",
+
+    projectId: "scriptlook-1e871",
+
+    storageBucket: "scriptlook-1e871.appspot.com",
+
+    messagingSenderId: "828092571172",
+
+    appId: "1:828092571172:web:bd5cde9771e6cd6de742b1"
+
+  
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
